c596bcebe5e23960611e917f5e7bfd48377a2109
lalon147 <lalon.rahman147@gmail.com>
2022-11-09 09:25:17 +0600

    footer updated

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -3,10 +3,13 @@ import React from 'react';
 const Footer = () => {
     return (
          <div>
-          <footer class="bg-gray-200 text-center lg:text-left">
-  <div class="text-gray-700 text-center p-4" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
-    © 2021 Copyright:
-    <a class="text-gray-800" href="https://tailwind-elements.com/">Tailwind Elements</a>
+          <footer className="bg-gray-200 text-center lg:text-left">
+  <div className="text-gray-700 text-center p-4" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
+   <div className='w-14 h-14 rounded-full bg-slate-300 flex items-center justify-center mr-2'>
+            <h1 className='text-black font-bold text-4xl  '>RC</h1>
+             
+          </div><p>RISAN's CEREOMONY</p>      © 2022 Copyright:
+    <a className="text-gray-800" href="https://tailwind-elements.com/">Tailwind Elements</a>
   </div>
 </footer>
          </div>
